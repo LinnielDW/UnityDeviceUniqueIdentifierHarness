2d8bf8ef30607562f5103cc002719bc049c4d92d
Linniel Driver-Williams <linnieldw@googlemail.com>
2020-10-09 21:37:11 +0200

    init

---
+++ Assets/Scripts/ButtonScripts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonScripts : MonoBehaviour
+{
+
+    public Button yourButton;
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		CopyStringToClipboard(SystemInfo.deviceUniqueIdentifier);
+	}
+
+    public void CopyStringToClipboard(string str)
+    {
+        GUIUtility.systemCopyBuffer = str;
+        yourButton.GetComponentInChildren<TMP_Text>().text = "Copied";
+        
+    }
+}
---
+++ Assets/Scripts/SystemInfoScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class SystemInfoScript : MonoBehaviour {
+    public TMP_Text  description;
+
+    void Start() {
+        description.text = SystemInfo.deviceUniqueIdentifier;
+    }
+}
